1e166358a334a3d9b131b9e4bee612a4e15ad11a
PJJSB <pieter199810@gmail.com>
2021-11-22 14:12:30 +0100

    Add Audiomanager and playerfootsteps

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    private static AudioManager instance;
+    private float volume = 0.2f;
+
+    private AudioSource playerAudioSource;
+    public GameObject player;
+    public List<AudioClip> footsteps;
+
+    //class cannot be instantiated because we only want ONE manager
+    //if you need the audiomanager, use AudioManager.GetInstance()
+    private AudioManager()
+    {
+
+    }
+
+    void Start()
+    {
+        instance = this;
+        playerAudioSource = player.GetComponent<AudioSource>();
+    }
+
+    public static AudioManager GetInstance()
+    {
+        return instance;
+    }
+
+    public void PlayFootstep()
+    {
+        //set random footstep
+        int i = Random.Range(0, 4);
+        playerAudioSource.PlayOneShot(footsteps[i], volume);
+    }
+}
